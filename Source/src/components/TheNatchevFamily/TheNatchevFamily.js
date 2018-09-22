8704973d55b0e58f231c3d2457c87e54c440e8a7
Plamen Natchev <40413994+PNatchev@users.noreply.github.com>
2018-09-21 21:34:05 -0500

    Add files via upload

---
+++ Source/src/components/TheNatchevFamily/TheNatchevFamily.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const tnf = (props) => {
+return(
+<div>
+  <p> The Natchev Family </p>
+</div>
+);
+
+}
+
+export default tnf;
---
+++ Source/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App title="Plamen Natchev" />, document.getElementById('root'));
+registerServiceWorker();
