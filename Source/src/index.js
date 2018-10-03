acb5e3278988cd700c2d10b8bccd1ce84d4dbae0
Plamen Natchev <40413994+PNatchev@users.noreply.github.com>
2018-10-03 16:26:01 -0500

    Delete index.js

--- Source/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App title="Plamen Natchev" />, document.getElementById('root'));
-registerServiceWorker();
